8ebb9343051439b60acc090e2d22f70b4e4f496f
gavin.liu <836276616@qq.com>
2019-09-24 14:07:55 +0800

    增加登录参数

--- SimonBus_Config.Web/ViewModels/LoginResponse.cs
+++ SimonBus_Config.Web/ViewModels/LoginResponse.cs
@@ -7,5 +7,14 @@ namespace SimonBus_Config.Web.ViewModels
 {
     public class LoginResponse
     {
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string loginname { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string password { get; set; }
     }
 }
